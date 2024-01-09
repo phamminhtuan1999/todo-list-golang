c51af409189e2901d675e1ca913d13f2e82679de
matthew <tuan_pham@datahouse.com>
2024-01-09 16:39:55 +0700

    feat: init source

---
+++ main.go
@@ -0,0 +1,336 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+type ToDoItem struct {
+	Id          int        `json:"id" gorm:"column:id"`
+	Title       string     `json:"title" gorm:"column:title"`
+	Description string     `json:"description" gorm:"column:description"`
+	Status      string     `json:"status" gorm:"column:status"`
+	CreatedAt   *time.Time `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty" gorm:"column:updated_at"`
+}
+
+func (ToDoItem) TableName() string {
+	return "todo_items"
+}
+
+type TodoItemCreation struct {
+	Id          int    `json:"-" gorm:"column:id" uri:"id"`
+	Title       string `json:"title" gorm:"column:title"`
+	Description string `json:"description" gorm:"column:description"`
+}
+
+func (TodoItemCreation) TableName() string {
+	return ToDoItem{}.TableName()
+}
+
+type TodoItemUpdate struct {
+	Title       *string `json:"title" gorm:"column:title"`
+	Description *string `json:"description" gorm:"column:description"`
+	Status      *string `json:"status" gorm:"column:status"`
+}
+
+func (TodoItemUpdate) TableName() string {
+	return ToDoItem{}.TableName()
+}
+
+type Paging struct {
+	Page  int   `json:"page" form:"page"`
+	Limit int   `json:"limit" form:"limit"`
+	Total int64 `json:"total" form:"-"`
+}
+
+func (p *Paging) Process() {
+	if p.Page <= 0 {
+		p.Page = 1
+	}
+
+	if p.Limit <= 0 || p.Limit >= 100 {
+		p.Limit = 10
+	}
+}
+
+func main() {
+	dsn := os.Getenv("DB_CONN_STR")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//CRUD: create, read, update, delete
+	r := gin.Default()
+
+	v1 := r.Group("/v1")
+	{
+		items := v1.Group("/items")
+		{
+			items.POST("", CreateItem(db))
+			items.GET("", GetItems(db))
+			items.GET("/:id", GetItem(db))
+			items.PATCH("/:id", UpdateItem(db))
+			items.DELETE("/:id", DeleteItem(db))
+		}
+	}
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Server is started!!!",
+		})
+	})
+
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080
+
+}
+
+func CreateItem(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data TodoItemCreation
+
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		if err := db.Create(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data.Id,
+		})
+	}
+}
+
+func GetItem(db *gorm.DB) func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		var data ToDoItem
+
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		if err := db.First(&data, id).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
+		})
+	}
+}
+
+func UpdateItem(db *gorm.DB) func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		var data TodoItemUpdate
+
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		if err := db.Where("id = ?", id).Updates(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": true,
+		})
+	}
+}
+
+func DeleteItem(db *gorm.DB) func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		//if err := db.Table(ToDoItem{}.TableName()).Where("id = ?", id).Delete(nil).Error; err != nil {
+		//	c.JSON(http.StatusBadRequest, gin.H{
+		//		"error": err.Error(),
+		//	})
+		//
+		//	return
+		//}
+
+		if err := db.Table(ToDoItem{}.TableName()).Where("id = ?", id).Updates(map[string]interface{}{
+			"status": "Deleted",
+		}).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": true,
+		})
+	}
+}
+
+func GetItems(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var paging Paging
+
+		if err := c.ShouldBind(&paging); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		paging.Process()
+
+		var result []ToDoItem
+
+		db = db.Where("status <> ?", "Deleted")
+
+		if err := db.Table(ToDoItem{}.TableName()).Count(&paging.Total).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		if err := db.Order("id desc").
+			Offset((paging.Page - 1) * paging.Limit).
+			Limit(paging.Limit).
+			Find(&result).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data":  result,
+			"total": paging,
+		})
+	}
+}
+
+type Student struct {
+	firstName string
+	lastName  string
+	email     string
+}
+
+func demoPointer() {
+	// bien, kieu du lieu, oop
+
+	// slice, array, map
+	// demoStructure()
+
+	st := Student{
+		firstName: "tuan",
+		lastName:  "pham",
+		email:     "tuan_pham@datahouse.com",
+	}
+
+	e := st.getEmail()
+
+	fmt.Println(e)
+	fmt.Println(st.email)
+}
+
+func (st *Student) getEmail() string {
+	st.email = "student@dha.com"
+	return st.email
+}
+
+func demoStructure() {
+	s := make([]string, 0)
+	s = append(s, "a")
+	s = append(s, "b")
+	s = append(s, "c")
+	fmt.Println(s)
+
+	m := make(map[string]int)
+	m["age"] = 25
+	fmt.Println(m["age"])
+
+	arr := [1]string{}
+	arr[0] = "hello"
+	fmt.Println(arr[0])
+}
+
+func demoVariable() {
+	var email string = "tuan_pham@datahouse.com"
+	fmt.Printf("%T", email)
+	fmt.Println()
+}
+
+//jsonData, err := json.Marshal(item)
+//
+//if err != nil {
+//	fmt.Println(err)
+//	return
+//}
+//
+//fmt.Println(string(jsonData))
+//
+//jsonStr := "{\"id\":0,\"title\":\"This is item 1\",\"description\":\"This is item 1 description\",\"status\":\"Doing\",\"created_at\":\"2024-01-08T16:53:54.942405Z\",\"updated_at\":null}"
+//
+//var item2 ToDoItem
+//
+//if err := json.Unmarshal([]byte(jsonStr), &item2); err != nil {
+//	fmt.Println(err)
+//	return
+//}
+//
+//fmt.Println(item2)
